a3496e9331b8cdc61d32baae541645d8ad3b4f69
hamada <hamada.devlop@gmail.com>
2022-05-30 22:11:44 +0200

    add_marker_on_map

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -9,16 +11,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  var marker = HashSet<Marker>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const GoogleMap(
-        initialCameraPosition: CameraPosition(
+      body: GoogleMap(
+        initialCameraPosition: const CameraPosition(
             target: LatLng(37.42796133580664, -122.085749655962),
-          zoom: 19,
-
         ),
+        markers: marker,
+        onMapCreated: (GoogleMapController googleMapController){
+          setState((){
+            marker.add(
+              const Marker(
+                markerId: MarkerId('1'),
+                position: LatLng(37.42796133580664, -122.085749655962),
+              ),
+            );
+          });
+        },
       ),
     );
   }
